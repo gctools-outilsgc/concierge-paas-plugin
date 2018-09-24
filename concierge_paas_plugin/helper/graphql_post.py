7c2533660ce0d43f9b41497839cc73ecfc596209
Unknown <guillaumelebeltech@gmail.com>
2018-09-24 09:53:35 -0400

    Integrate some code related to OIDC
    Create model_configuration helper where queries related to the Configuration are added.
    Integrate the code from OIDC which claims base queries are used.

--- concierge_paas_plugin/helper/graphql_post.py
+++ concierge_paas_plugin/helper/graphql_post.py
@@ -1,12 +1,12 @@
 import requests
-from concierge_paas_plugin.models import Configuration
+from concierge_paas_plugin.helper import model_configuration
 
 def executeQuery(query):
     retry = 3
     success = False
     attempt = 0
 
-    configuration = Configuration.objects.get(default=True)
+    configuration = model_configuration.getdefault()
     while not success and attempt < retry:
         response = requests.post(configuration.end_point, headers={'Authorization': 'Token ' + configuration.token}, data=query)
         if not response.status_code == requests.codes.ok:
--- concierge_paas_plugin/views.py
+++ concierge_paas_plugin/views.py
@@ -1,14 +1,11 @@
 import requests
 from django.contrib.auth.decorators import login_required
-from .helper import graphql_post
 from oidc_provider.lib.claims import ScopeClaims
-
-from .models import Configuration
-from .helper import graphql_query, graphql_mutation
+from .helper import graphql_query, graphql_mutation, graphql_post, model_configuration
 
 @login_required
 def create_profile(request, id, name, email):
-    configuration = Configuration.objects.get(default=True)
+    configuration = model_configuration.getdefault()
 
     # ToDo replace this with messages over Kafka system for notification
     if configuration.trigger is True:
@@ -37,93 +34,92 @@ def queryprofile(request, userId):
     return graphql_post.executeQuery(query)
 
 
-# def userinfo(claims, user):
-#     """
-#     Populate claims dict.
-#     """
-#     retrys = 3
-#
-#     claims['name'] = user.name
-#     claims['email'] = user.email
-#
-#     if settings.GRAPHQL_TRIGGERS:
-#         return claimsfromprofiles(retrys, claims)
-#     else:
-#         return claims
+def userinfo(claims, user):
+    """
+    Populate claims dict.
+    """
+    retrys = 3
+
+    claims['name'] = user.name
+    claims['email'] = user.email
+
+    if model_configuration.getdefault().trigger:
+        return claimsfromprofiles(retrys, claims)
+    else:
+        return claims
+
+def claimsfromprofiles(retry, claims):
+    response = queryprofile(retry)
+
+    if response is None:
+        return claims
+
+    try:
+        profileclaims = claims
+        response_address = response['data']['profiles'][0].get('address')
+
+        profileclaims['picture'] = checkvalue(response['data']['profiles'][0].get('avatar'))
+        if response['data']['profiles'][0].get('mobilePhone') is not None:
+            profileclaims['phone_number'] = checkvalue(response['data']['profiles'][0].get('mobilePhone'))
+        else:
+            profileclaims['phone_number'] = checkvalue(response['data']['profiles'][0].get('officePhone'))
+
+        if response_address is not None:
+
+            if 'streetAddress' in response_address:
+                profileclaims['address']['street_address'] = checkvalue(response_address.get('streetAddress'))
+            if 'city' in response_address:
+                profileclaims['address']['locality'] = checkvalue(response_address.get('city'))
+            if 'province' in response_address:
+                profileclaims['address']['region'] = checkvalue(response_address.get('province'))
+            if 'postalCode' in response_address:
+                profileclaims['address']['postal_code'] = checkvalue(response_address.get('postalCode'))
+            if 'country' in response_address:
+                profileclaims['address']['country'] = checkvalue(response_address.get('country'))
+    except Exception:
+        return claims
+
+    return profileclaims
 
 
-# def claimsfromprofiles(retry, claims):
-#     response = queryprofile(retry)
-#
-#     if response is None:
-#         return claims
-#
-#     try:
-#         profileclaims = claims
-#         response_address = response['data']['profiles'][0].get('address')
-#
-#         profileclaims['picture'] = checkvalue(response['data']['profiles'][0].get('avatar'))
-#         if response['data']['profiles'][0].get('mobilePhone') is not None:
-#             profileclaims['phone_number'] = checkvalue(response['data']['profiles'][0].get('mobilePhone'))
-#         else:
-#             profileclaims['phone_number'] = checkvalue(response['data']['profiles'][0].get('officePhone'))
-#
-#         if response_address is not None:
-#
-#             if 'streetAddress' in response_address:
-#                 profileclaims['address']['street_address'] = checkvalue(response_address.get('streetAddress'))
-#             if 'city' in response_address:
-#                 profileclaims['address']['locality'] = checkvalue(response_address.get('city'))
-#             if 'province' in response_address:
-#                 profileclaims['address']['region'] = checkvalue(response_address.get('province'))
-#             if 'postalCode' in response_address:
-#                 profileclaims['address']['postal_code'] = checkvalue(response_address.get('postalCode'))
-#             if 'country' in response_address:
-#                 profileclaims['address']['country'] = checkvalue(response_address.get('country'))
-#     except Exception:
-#         return claims
-#
-#     return profileclaims
+def checkvalue(stringvalue):
+    if stringvalue is not None:
+        return stringvalue
+    else:
+        return ''
 
 
-# def checkvalue(stringvalue):
-#     if stringvalue is not None:
-#         return stringvalue
-#     else:
-#         return ''
+class CustomScopeClaims(ScopeClaims):
+    info_modify_profile = (
+        _('Profile Modification'),
+        _('Ability to view and modify your profile information'),
+    )
 
+    def scope_modify_profile(self):
+        # self.user - Django user instance.
+        # self.userinfo - Dict returned by OIDC_USERINFO function.
+        # self.scopes - List of scopes requested.
+        # self.client - Client requesting this claims.
+        dic = {
+            'modify_profile': 'True',
+            'detailed_profile': 'True'
+        }
 
-# class CustomScopeClaims(ScopeClaims):
-#     info_modify_profile = (
-#         _('Profile Modification'),
-#         _('Ability to view and modify your profile information'),
-#     )
-#
-#     def scope_modify_profile(self):
-#         # self.user - Django user instance.
-#         # self.userinfo - Dict returned by OIDC_USERINFO function.
-#         # self.scopes - List of scopes requested.
-#         # self.client - Client requesting this claims.
-#         dic = {
-#             'modify_profile': 'True',
-#             'detailed_profile': 'True'
-#         }
-#
-#         return dic
-#
-#     info_profile = (
-#         _('Basic profile'),
-#         _('Access to your name, email, avatar, and address'),
-#     )
-#
-#     info_detailed_profile = (
-#         _('Detailed User profile'),
-#         _('Access to your name, email, avatar, address, and organization information'),
-#     )
-#
-#     def scope_detailed_profile(self):
-#         dic = {
-#             'detailed_profile': 'True',
-#         }
-#
-#         return dic
+        return dic
+
+    info_profile = (
+        _('Basic profile'),
+        _('Access to your name, email, avatar, and address'),
+    )
+
+    info_detailed_profile = (
+        _('Detailed User profile'),
+        _('Access to your name, email, avatar, address, and organization information'),
+    )
+
+    def scope_detailed_profile(self):
+        dic = {
+            'detailed_profile': 'True',
+        }
+
+        return dic
